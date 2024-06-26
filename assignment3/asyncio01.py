6b03da19f48733f1ba87aa5b1ef199caa6627a47
WeerachaiSa <inkernovy15@gmail.com>
2024-06-26 11:32:13 +0700

    commitprocess

--- assignment3/asyncio01.py
+++ assignment3/asyncio01.py
@@ -1,1 +1,12 @@
 # check the type of a coroutine
+import asyncio
+# definie a coroutine
+async def custom_coro():
+    #await another coroutine
+    await asyncio.sleep(1)
+
+# create a new coroutine
+coro = custom_coro()
+#check the type of the coroutine
+print(type(coro))
+
